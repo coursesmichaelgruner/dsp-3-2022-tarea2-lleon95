ea01ecdbcd9c56a27e6f0fc8783f1eb6dd20cb5b
Luis Leon <lleon95@gmail.com>
2022-10-01 08:10:25 -0600

    Fix the exercise 3 by adding the resampler

--- hw2/generator.py
+++ hw2/generator.py
@@ -5,7 +5,8 @@ import numpy as np
 import sys
 
 
-def generate_440Hz(samples_per_period):
+
+def generate_440Hz(loc='input.wav'):
     # The number of samples per period is given by the quotient of fs/f
     # since f = periods/s, and fs = samples/s. Then fs/f = samples/period
     duration = 3
@@ -14,12 +15,13 @@ def generate_440Hz(samples_per_period):
     fi = 440
 
     # Compute the sampling rate
-    fsi = samples_per_period * fi
+    fsi = 44100
 
     # Log info
+    print("Generating:")
     print(f"Sine frequency: {fi} Hz")
-    print(f"Samples per period: {samples_per_period}")
-    print(f"Computed Sampling Frequency: {fsi} Hz")
+    print(f"Samples per period: {fsi / fi}")
+    print(f"Sampling Frequency: {fsi} Hz")
 
     # Create the input
     nsamples = duration * fsi
@@ -27,11 +29,38 @@ def generate_440Hz(samples_per_period):
     samples = list(np.sin(2 * np.pi * (fi / fsi) * x))
 
     # Let's write the output
-    audio.write_audio_file('sample.wav', samples, fsi)
+    audio.write_audio_file(loc, samples, fsi)
 
     # Play back both
-    print("Playing the wave")
-    audio.play_audio('sample.wav')
+    print("Playing the input wave")
+    audio.play_audio(loc)
+
+def resample_440Hz(samples_per_period, inputloc='input.wav', outputloc='output.wav'):
+    # The number of samples per period is given by the quotient of fs/f
+    # since f = periods/s, and fs = samples/s. Then fs/f = samples/period
+    duration = 3
+
+    # Input
+    fi = 440
+
+    # Compute the sampling rate
+    fso = samples_per_period * fi
+
+    # Log info
+    print(f"Sine frequency: {fi} Hz")
+    print(f"Samples per period: {samples_per_period}")
+    print(f"Computed Sampling Frequency: {fso} Hz")
+
+    # Read the input
+    # Read audio file
+    samples, _ = audio.read_audio_file(inputloc)
+
+    # Let's write the output
+    audio.write_audio_file(outputloc, samples, fso)
+
+    # Play back both
+    print("Playing the output wave")
+    audio.play_audio(outputloc)
 
 
 if __name__ == "__main__":
@@ -40,4 +69,5 @@ if __name__ == "__main__":
         print("Usage:")
         print("\t./generator.py SAMPLES_PER_PERIOD")
     else:
-        generate_440Hz(float(sys.argv[1]))
+        generate_440Hz()
+        resample_440Hz(float(sys.argv[1]))
